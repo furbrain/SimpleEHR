6ecc62d35bc180a7e43fa904a4061eb14758a00c
Phil Underwood <beardydoc@gmail.com>
2023-10-29 21:24:18 +0000

    Make the serialisers a bit more computer friendly

--- EHR/serialisers.py
+++ EHR/serialisers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Person, Code
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
+class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ['given', 'family', 'nhs_number', 'birthdate', 'code_set']
+        fields = ['id', 'given', 'family', 'nhs_number', 'birthdate', 'code_set']
+        depth = 1
 
-class CodeSerializer(serializers.HyperlinkedModelSerializer):
+class CodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Code
         fields = ['concept_code', 'term_code', 'text', 'person', 'significant','active','date']
